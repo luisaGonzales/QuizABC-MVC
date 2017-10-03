8f9030e17fbbb598f8a552853a737585ba8f280e
luisaGonzales <luisagc711@gmail.com>
2017-10-02 20:21:26 -0500

    Avance de respuestas

--- app.jsx
+++ app.jsx
@@ -1,54 +1,35 @@
 let preguntas = [{
       pregunta: `¿Cuál es la aerolínea más antigua del mundo?`,
-      opciones: {
-            A: "Avianca",
-            B: "KLM",
-            C: "Qantas"
-      },
+      opciones: ["Avianca", "KLM", "Qantas"],
       correcta: "KLM",
       imagen: "img/plane.svg"
 },
 {
       pregunta: `¿Cuál es el puerto más grande del mundo?`,
-      opciones: {
-            A: "Puerto de Singapur",
-            B: "Puerto de Rotterdam",
-            C: "Puerto de Shangai"
-      },
+      opciones: ["Puerto de Singapur", "Puerto de Rotterdam", "Puerto de Shangai"],
       correcta: "Puerto de Shangai",
       imagen: "img/ship.svg"
 },
 {
       pregunta: `¿Cuál es la distancia más larga en bicicleta hacia atrás?`,
-      opciones: {
-            A: "89.30 km",
-            B: "675.10 km",
-            C: "337.60 km"
-      },
+      opciones: ["89.30 km", "675.10 km", "337.60 km" ],
       correcta: "337.60 km",
       imagen: "img/bycicle.svg"
 },
 {
       pregunta: `¿Cuál es la velocidad más alta alcanzada nunca por un autobús escolar?`,
-      opciones: {
-            A: "590 km/h",
-            B: "320 km/h",
-            C: "245 km/h"
-      },
+      opciones: ["590 km/h", "320 km/h", "245 km/h"],
       correcta: "590 km/h",
       imagen: "img/bus.svg"
 },
 {
       pregunta: `¿Cuál es el viaje más largo con un tanque de gas?`,
-      opciones: {
-            A: "2,617 km",
-            B: "3,568 km",
-            C: "1,732 km"
-      },
+      opciones: ["2,617 km", "3,568 km", "1,732 km" ],
       correcta: "2,617 km",
       imagen: "img/car.svg"
 }
 ];
+
 
 
 class Model {
@@ -67,20 +48,41 @@ class Model {
    inform() {
       this.render();
    }
-
-
+   aumentar(){
+         this.preguntaActual = this.preguntaActual + 1;
+         this.inform();
+   }
+   seleccionar(e){
+            let respuesta = e.target.id;
+            console.log(respuesta);
+         this.aumentar();
+   }
 }
 
 const App = ({ title, model }) => {
+      const items = model.preguntas[model.preguntaActual].opciones.map((alternativa, index) => {
+            return (
+               <div>
+                  <button id={index} onClick={e => {model.seleccionar(e)}}>{alternativa}</button>
+               </div>
+            );
+         });
+
       return (
             <div>
-                  <h1>Quiz ABC</h1>
+                  <section>
+                        <img src={model.preguntas[model.preguntaActual].imagen} alt="imagen"/>
+                  </section>
+                  <div>
+                        <div>{items}</div>
+                  </div>
             </div>
       );
 };
 
 
 let model = new Model();
+console.log(model);
 let render = () => {
    ReactDOM.render(
       <App model={model}  />,
